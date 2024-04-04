1d043fab18d5147121c6bfbed83ed968fcd95180
suvam0455 <40341738+suvam0455@users.noreply.github.com>
2024-04-04 20:49:08 +0000

    Pending changes exported from your codespace

---
+++ mern-booking-app/frontend/src/AppRoutes.tsx
@@ -0,0 +1,15 @@
+import {Navigate ,Routes,Route} from "react-router-dom"
+
+const AppRoutes = () => {
+    
+    return(
+        <Routes>
+            <Route path ="/" element={<span>Home Page</span>}/>
+            <Route path ="/user-profile" element={<span>User Profile </span>}/>
+            <Route path ="*" element = {<Navigate to ="/" />} />
+        </Routes>
+    );
+
+};
+
+export default AppRoutes;

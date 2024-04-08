9d9300372081104d2c9713f44547afb5c4e8188c
suvam0455 <40341738+suvam0455@users.noreply.github.com>
2024-04-08 15:06:09 +0000

    Pending changes exported from your codespace

---
+++ mern-booking-app/backend/src/index.ts
@@ -0,0 +1,16 @@
+import express,{Request ,Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to DataBase"))
+const app = express();
+app.use(express.json());
+app.use (cors())
+
+app.get("/test",async(req:Request,res:Response)=>{
+    res.json({messge:"hello"});
+})
+app.listen(7000,() => {
+    console.log("server started at localhost 7000");
+});

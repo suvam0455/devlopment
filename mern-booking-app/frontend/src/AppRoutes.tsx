f7b342723be6dd875cc2bd9bc54d47c8f10c01ba
suvam0455 <suvam0455@gmail.com>
2024-04-08 20:18:23 +0000

    done

--- mern-booking-app/frontend/src/AppRoutes.tsx
+++ mern-booking-app/frontend/src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import {Navigate ,Routes,Route} from "react-router-dom"
 import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => {
     
     return(
         <Routes>
-            <Route path ="/" element={<Layout>HOME PAGE</Layout>}/>
+            <Route path ="/" element={<Layout><HomePage /></Layout>}/>
             <Route path ="/user-profile" element={<span>User Profile </span>}/>
             <Route path ="*" element = {<Navigate to ="/" />} />
         </Routes>

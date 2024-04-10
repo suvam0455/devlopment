3e9e34d524779830c2f54d0b0a53639a41d9c1cf
suvam0455 <suvam0455@gmail.com>
2024-04-10 20:43:31 +0000

    new files again

--- mern-booking-app/backend/src/index.ts
+++ mern-booking-app/backend/src/index.ts
@@ -2,15 +2,16 @@ import express,{Request ,Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to DataBase"))
 const app = express();
 app.use(express.json());
 app.use (cors())
 
-app.get("/test",async(req:Request,res:Response)=>{
-    res.json({messge:"hello"});
-})
+app.use("/api/my/user", myUserRoute)
+
+
 app.listen(7000,() => {
     console.log("server started at localhost 7000");
 });
